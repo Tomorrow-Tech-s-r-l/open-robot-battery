04fb7740440185fea1b075c8757b77d83c3e85b6
giulio <peloso.giul@gmail.com>
2025-10-31 14:34:53 +0100

    Updated KiCad project files and documentation

--- software/ros2/src/orb_i2c_support/orb_i2c_support/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Utilities and ROS 2 nodes for the ORB I2C interface."""
-
-from .backend_factory import create_backend  # noqa: F401
-from .i2c_backend import I2CBackend, SimulatedI2CBackend  # noqa: F401
-from .ez_pd_bcr_backend import EZPDBCRI2CBackend  # noqa: F401
